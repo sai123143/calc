557cccd285d17135b9186021b53c08220a4e4c0e
SAITEJA JINDE <80438880+sai123143@users.noreply.github.com>
2025-05-23 22:40:52 +0530

    Update and rename README.md to calc.py

---
+++ calc.py
@@ -0,0 +1,24 @@
+while True:
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operator = input("Enter an operator (+, -, *, /): ")
+
+    if operator == '+':
+        result = num1 + num2
+    elif operator == '-':
+        result = num1 - num2
+    elif operator == '*':
+        result = num1 * num2
+    elif operator == '/':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = "Error: Division by zero"
+    else:
+        result = "Invalid operator"
+
+    print("Result:", result)
+
+    again = input("Do you want to calculate again? (yes/no): ")
+    if again.lower() != "yes":
+        break
